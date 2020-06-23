ccb1d83120753cd9f22c1cf69af061976b97d6f9
Silmaen <38850858+Silmaen@users.noreply.github.com>
2020-06-23 22:24:45 +0200

    automatize load of python files (#8)

--- p00_30seconds/__init__.py
+++ p00_30seconds/__init__.py
@@ -1,3 +1,17 @@
 
-__all__ = ["connexion_check", "connexion_table"]
-
+def run(dry_run: bool = False):
+    """
+    run all submodules
+    """
+    from pkgutil import iter_modules
+    from pathlib import Path
+    from importlib import import_module
+    exec_status = {}
+    for (_, name, _) in iter_modules([Path(__file__).parent]):
+        try:
+            import_module('.' + name, package=__name__).main(dry_run)
+        except Exception as err:
+            exec_status[name] = "Exception: " + str(err)
+            continue
+        exec_status[name] = "Ok"
+    return exec_status
---
+++ test.py
@@ -0,0 +1,8 @@
+"""
+main entry point for testing
+"""
+from tests import *
+import unittest
+
+if __name__ == "__main__":
+    unittest.main()
